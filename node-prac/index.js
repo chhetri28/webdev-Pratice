afff94f32a81543bc6ee9279cebca3bea35c3fc1
chhetri28 <chettrisourav28@gmail.com>
2022-08-06 21:47:54 +0530

    REPL

--- node-prac/index.js
+++ node-prac/index.js
@@ -12,3 +12,11 @@ http.createServer(function (req, res) {
 	res.end('Hello World!');
 
 }).listen(8080);
+//Node.js REPL (READ, EVAL, PRINT, LOOP)
+
+/*
+Read : It reads the inputs from users and parses it into JavaScript data structure. It is then stored to memory.
+Eval : The parsed JavaScript data structure is evaluated for the results.
+Print : The result is printed after the evaluation.
+Loop : Loops the input command. To come out of NODE REPL, press ctrl+c twice
+*/
