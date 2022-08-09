49ddfd8775f52eb94bf03679574d8e7b860d22f9
chhetri28 <chettrisourav28@gmail.com>
2022-08-09 20:32:01 +0530

    modules types

--- node-prac/index.js
+++ node-prac/index.js
@@ -34,4 +34,47 @@ Node.js Modules
 In Node.js, Modules are the blocks of encapsulated code that communicates with an external application on the basis of their related functionality. 
 Modules can be a single file or a collection of multiples files/folders. 
 The reason programmers are heavily reliant on modules is because of their re-usability as well as the ability to break down a complex piece of code into manageable chunks.
+
+Modules are of three types:
+
+Core Modules
+local Modules
+Third-party Modules
+
+Core Modules: Node.js has many built-in modules that are part of the platform and comes with Node.js installation. These modules can be loaded into the program by using the require function.
+
+Syntax:
+
+var module = require('module_name');
+The require() function will return a JavaScript type depending on what the particular module returns. 
+The following example demonstrates how to use the Node.js Http module to create a web server.
+
+
+Local Modules: Unlike built-in and external modules, local modules are created locally in your Node.js application. 
+Let’s create a simple calculating module that calculates various operations. 
+Create a calc.js file that has the following code:
+exports.add = function (x, y) {
+	return x + y;
+};
+	
+exports.sub = function (x, y) {
+	return x - y;
+};
+	
+exports.mult = function (x, y) {
+	return x * y;
+};
+	
+exports.div = function (x, y) {
+	return x / y;
+};
+Third-party modules: Third-party modules are modules that are available online using the Node Package Manager(NPM). 
+These modules can be installed in the project folder or globally. Some of the popular third-party modules are mongoose, express, angular, and react.
+
+Example:
+
+npm install express
+npm install mongoose
+npm install -g @angular/cli
+
 */
